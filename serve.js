e990d60f11b431192262a248a3aa854496c04f6d
rhodey <notrhodey@protonmail.com>
2018-08-18 15:55:46 +0200

    ok

--- serve.js
+++ serve.js
@@ -16,7 +16,7 @@ function serveHTTP(port) {
 }
 
 app.post('/join', function (req, res) {
-  console.log(`alias ${req.body.alias} wanna join with pubkey ${req.body.key} from ip ${req.ip}:${req.body.port}`)
+  console.log(`join request from ${req.ip}:${req.body.port} with pubkey ${req.body.pubkey}`)
   res.status(204).send()
 })
 
