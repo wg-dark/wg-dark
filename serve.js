48ad0b22c5e299ebc19ec3174408189b2ed32977
rhodey <notrhodey@protonmail.com>
2018-08-18 06:32:00 -0700

    json stuff

--- serve.js
+++ serve.js
@@ -1,8 +1,10 @@
-const http = require('http')
+const http    = require('http')
+const parser  = require('body-parser')
 const express = require('express')
 
 const app = express()
 const argv = require('minimist')(process.argv.slice(2))
+app.use(parser.json())
 
 function serveHTTP(port) {
   return new Promise((res, rej) => {
@@ -14,7 +16,8 @@ function serveHTTP(port) {
 }
 
 app.post('/join', function (req, res) {
-  res.send('joinnn')
+  console.log(`alias ${req.body.alias} wanna join with pubkey ${req.body.key}`)
+  res.status(204).send()
 })
 
 app.get('/status', function (req, res) {
