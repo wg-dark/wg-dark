d2656b838032f5e5e55318f9537d6256edb60c1f
Jake McGinty <me@jake.su>
2018-08-19 00:03:42 +0200

    fix up some shiiiit

--- wg-test.js
+++ wg-test.js
@@ -2,15 +2,22 @@ const Wg = require('./wg');
 
 (async function() {
     const wg = new Wg("blah");
-    let config = await wg.getPeersConfig();
-    console.log("got config");
-    console.log(config);
+    await wg.up();
 
-    wg.addConfig(`
+    await wg.addConfig(`
     [Peer]
-    PublicKey = DfyxzL1mtigKIfM51OhxD+FBBSk+/SHEUE9UlDJ45W4=
+    PublicKey = AAAAAL1mtigKIfM51OhxD+FBBSk+/SHEUE9UlDJ45W4=
     AllowedIPs = 1.0.0.0/1
     Endpoint = 185.65.135.131:51820
-    `);
+    `)
+
+    await wg.addPeer({
+        pubkey: "BBBBBL1mtigKIfM51OhxD+FBBSk+/SHEUE9UlDJ45W4=",
+        allowedIPs: "10.13.37.3/32"
+    })
+
+    let config = await wg.getPeersConfig()
+    console.log("got config")
+    console.log(config)
 })();
 
--- wg.js
+++ wg.js
@@ -2,43 +2,52 @@ const execFile = require('child_process').execFile;
 const tmp = require('tmp');
 const fs = require('fs');
 
+async function execAsync(cmd, args) {
+  return new Promise((resolve, reject) => {
+    execFile(cmd, args, (error, stdout, stderr) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(stdout)
+      }
+    })
+  })
+}
+
 class Wg {
   constructor(iface) {
     this.iface = iface;
   }
 
   async up() {
-    let wg = this;
-    return new Promise((resolve, reject) => {
-      execFile("ip", ["link", "add", wg.iface, "type", "wireguard"], () => {
-        execFile("ip", ["route", "add", "10.13.37.0/24", "dev", wg.iface], () => {
-          resolve()
-        })
-      })
-    })
+    let wg = this
+    await execAsync("ip", ["link", "add", wg.iface, "type", "wireguard"])
+    await execAsync("ip", ["link", "set", "mtu", "1420", "dev", wg.iface])
+    await execAsync("ip", ["addr", "add", "10.13.37.1/24", "dev", wg.iface])
+    await execAsync("ip", ["link", "set", wg.iface, "up"])
+    // await execAsync("ip", ["route", "add", "10.13.37.0/24", "dev", wg.iface])
+  }
+
+  async down() {
+    let wg = this
+    await execAsync("ip", ["link", "del", "dev", wg.iface])
   }
 
   async getPeersConfig() {
-    let wg = this;
-    return new Promise((resolve, reject) => {
-      execFile("wg", ["showconf", wg.iface], (error, stdout, stderr) => {
-        if (error) {
-          reject(error);
-        }
+    let wg = this
 
-        const peers = stdout.toString('utf8')
-          .split("\n")
-          .filter((line) => {
-            return !line.trim().startsWith("[Interface]")
-              && !line.trim().startsWith("ListenPort")
-              && !line.trim().startsWith("FwMark")
-              && !line.trim().startsWith("PrivateKey")
-              && line !== ""
-          })
-          .join("\n");
-        resolve(peers);
-      });
-    });
+    let stdout = await execAsync("wg", ["showconf", wg.iface])
+
+    return stdout.toString('utf8')
+      .split("\n")
+      .filter((line) => {
+        return !line.trim().startsWith("[Interface]")
+          && !line.trim().startsWith("ListenPort")
+          && !line.trim().startsWith("FwMark")
+          && !line.trim().startsWith("PrivateKey")
+          && line !== ""
+      })
+      .join("\n")
   }
 
   /**
@@ -48,12 +57,8 @@ class Wg {
    * @param {string} peer.allowedIPs - The peer's allowed IPs
    */
   async addPeer(peer) {
-    let wg = this;
-    return new Promise((resolve, reject) => {
-      execFile("wg", ["set", wg.iface, "peer", peer.pubkey, "allowed-ips", peer.allowedIPs], () => {
-        resolve()
-      })
-    })
+    let wg = this
+    await execAsync("wg", ["set", wg.iface, "peer", peer.pubkey, "allowed-ips", peer.allowedIPs])
   }
 
   async addConfig(configString) {
@@ -65,19 +70,15 @@ class Wg {
           return;
         }
 
-        fs.write(fd, configString, (err) => {
+        fs.write(fd, configString, async (err) => {
           if (err) {
             reject(err);
             return;
           }
 
-          execFile("wg", ["addconf", wg.iface, path], (error, stdout, stderr) => {
-            if (error) {
-              reject(error);
-            }
-            cleanup();
-            resolve();
-          });
+          await execAsync("wg", ["addconf", wg.iface, path])
+          cleanup()
+          resolve()
         });
       });
     });
