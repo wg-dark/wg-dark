6d89e6d603e51b2db6b071d15947f94d64f2d3a1
Jake McGinty <me@jake.su>
2018-08-18 23:34:29 +0200

    some new untested wg stuff!

--- wg.js
+++ wg.js
@@ -7,10 +7,21 @@ class Wg {
     this.iface = iface;
   }
 
+  async up() {
+    let wg = this;
+    return new Promise((resolve, reject) => {
+      execFile("ip", ["link", "add", wg.iface, "type", "wireguard"], () => {
+        execFile("ip", ["route", "add", "10.13.37.0/24", "dev", wg.iface], () => {
+          resolve()
+        })
+      })
+    })
+  }
+
   async getPeersConfig() {
     let wg = this;
-    return new Promise(function (resolve, reject) {
-      execFile("wg", ["showconf", wg.iface], function (error, stdout, stderr) {
+    return new Promise((resolve, reject) => {
+      execFile("wg", ["showconf", wg.iface], (error, stdout, stderr) => {
         if (error) {
           reject(error);
         }
@@ -30,9 +41,24 @@ class Wg {
     });
   }
 
+  /**
+   *
+   * @param {Object} peer - The new peer's information
+   * @param {string} peer.pubkey - The public key
+   * @param {string} peer.allowedIPs - The peer's allowed IPs
+   */
+  async addPeer(peer) {
+    let wg = this;
+    return new Promise((resolve, reject) => {
+      execFile("wg", ["set", wg.iface, "peer", peer.pubkey, "allowed-ips", peer.allowedIPs], () => {
+        resolve()
+      })
+    })
+  }
+
   async addConfig(configString) {
     let wg = this;
-    return new Promise(function (resolve, reject) {
+    return new Promise((resolve, reject) => {
       tmp.file((err, path, fd, cleanup) => {
         if (err) {
           reject(err);
